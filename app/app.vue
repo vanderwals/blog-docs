2db9ac936360f69c2f49a93541b23bdd5fbb6c9e
eguo <guoshen@zhengyun.app>
2025-08-06 15:09:31 +0800

    在app.vue中添加了meta标签和标题配置，优化了页面的SEO和可访问性，提升用户体验。

--- app/app.vue
+++ app/app.vue
@@ -6,3 +6,17 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup>
+const appConfig = useAppConfig();
+useHead({
+  meta: [
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "description", content: appConfig.site.description },
+  ],
+  link: [{ rel: "icon", href: appConfig.site.logo.src }],
+  title: appConfig.homepage.hero.title,
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+</script>
